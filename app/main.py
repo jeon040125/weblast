8ebac56a040601558b45b5fb4fa4474b62bba02b
jeon040125 <109123763+jeon040125@users.noreply.github.com>
2024-04-12 17:45:32 +0900

    d
    d

--- app/main.py
+++ app/main.py
@@ -1,15 +1,56 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+user_name = None
+
+class User(BaseModel):
+    name: str
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return{ "message": "Hello Bosman!"}
+
+@app.get("/home")
+def home():
+    return { "message": "Bye Bosman!" }
+
+#####여기까지 저번주########
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/user/")
+async def receive_user(user: User):
+    global user_name
+    user_name = user.name
+    return {"message": "User name received"}
+
+
+@app.get("/user/")
+async def get_user():
+    return {"user_name": user_name}
+
+@app.put("/user/")
+async def receive_user(user: User):
+    global user_name
+    user_name = user.name
+    return {"message": "User name changed"}
+
+
+@app.delete("/user/")
+async def del_user():
+    global user_name
+    user_name = "DELETED"
+    return {"message": "User name deleted"}
+
+
+######
+
+# 클래스 쓰는 법
+
+user1=User(name="dongha hihi")
+
+
+
+###########
